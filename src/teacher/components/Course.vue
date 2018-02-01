7dd9eb70cda669914cfc9c5f65fe3eef86cff753
tan <1193728065@qq.com>
2018-02-01 18:05:59 +0800

    图片

---
+++ src/teacher/components/Course.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="">
+    <div class="topMenu"></div>
+    <div class="componentBox">课程</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Course',
+  data () {
+    return {
+      
+    }
+  },
+  mounted() {
+
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
