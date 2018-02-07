81b3daa06f8f1984b04235eb0bc5994803a89714
wangkang <1589920411@qq.com>
2018-02-07 09:05:37 +0800

    新增MyCenter

---
+++ src/student/components/MyCenter.vue
@@ -0,0 +1,22 @@
+ <template>
+	 <div>
+           <div class="">
+                                          个人中心
+           </div>
+	 </div>
+</template>
+<script>
+import casesindex  from '@/student/components/courseTraining/casesindex'
+  export default {
+    data() {
+      return {
+    
+      }
+    },
+  };
+</script>
+
+<style scoped>
+
+
+</style>
